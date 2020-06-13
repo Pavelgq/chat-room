b74848c70e7afb0528fa23db8e4bba2ceb3dbffc
PavelGQ <pavelgq@yandex.ru>
2020-06-13 23:41:07 +0300

    Попробовал web-socket

---
+++ server.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var Static = require('node-static');
+
+var WebSocketServer = new require('ws');
+
+// подключённые клиенты
+var clients = {};
+
+// WebSocket-сервер на порту 8081
+var webSocketServer = new WebSocketServer.Server({
+  port: 8081
+});
+webSocketServer.on('connection', function(ws) {
+
+  var id = Math.random();
+  clients[id] = ws;
+  console.log("новое соединение " + id);
+
+  ws.on('message', function(message) {
+    console.log('получено сообщение ' + message);
+
+    for (var key in clients) {
+      clients[key].send(message);
+    }
+  });
+
+  ws.on('close', function() {
+    console.log('соединение закрыто ' + id);
+    delete clients[id];
+  });
+
+});
